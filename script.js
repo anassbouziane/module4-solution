34d7531496e8a11b8b7e0ef4c30e657df7fd5beb
anassbouziane <95655253+anassbouziane@users.noreply.github.com>
2023-10-29 16:52:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 (function () {
-    var names = ["John", "Jane", "Jack", "Jill", "Mike", "Mary"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
     for (var i = 0; i < names.length; i++) {
-        var name = names[i];
-        var firstLetter = name.charAt(0).toLowerCase();
+        var firstLetter = names[i].charAt(0).toLowerCase();
 
         if (firstLetter === 'j') {
-            console.log("Goodbye " + name);
+            byeSpeaker.speak(names[i]);
         } else {
-            console.log("Hello " + name);
+            helloSpeaker.speak(names[i]);
         }
     }
 })();
